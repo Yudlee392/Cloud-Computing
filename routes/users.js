1fbc1c4b54efb771984f6d009eb610a01bdd5cb3
Phan Nhật Linh <93339285+Linhvjc@users.noreply.github.com>
2022-10-24 20:22:50 +0700

    fix

--- routes/users.js
+++ routes/users.js
@@ -67,9 +67,10 @@ router.post('/delete', async function (req, res, next) {
   let deleteProductId = req.body.delete
   let shop_id = session.shop_id;
   let role = session.role;
-  let tableString
+  TimeOut(1000)
   deleteProductModule(deleteProductId, shop_id)
-  tableString = await tableProductModule(shop_id, role)
+  TimeOut(2000)
+  let tableString = await tableProductModule(shop_id, role)
   res.render('users', {
     title: 'USER page',
     data: tableString,
@@ -96,7 +97,9 @@ router.post('/update', async function (req, res, next) {
     let name = req.body.name;
     let price = req.body.price;
     let quantity = req.body.quantity;
+    TimeOut(1000)
     updateProductModule(id, name, price, quantity, shop_id)
+    TimeOut(2000)
     tableString = await tableProductModule(shop_id, role)
     res.render('users', {
       title: 'USER page',
